6b197c25ea3369519b2c85ef210d4caa37249b10
Smidgeon <finlaynugent@gmail.com>
2022-07-09 22:07:12 +0100

    Initial Setup of base unity plugin
    to do: implementation

---
+++ BoltPatch.cs
@@ -0,0 +1,26 @@
+﻿namespace BoltActionFix
+{
+    using FistVR;
+    using HarmonyLib;
+    using UnityEngine;
+    
+    public class BoltPatch : MonoBehaviour
+    {
+        [HarmonyPatch(typeof(FVRPhysicalObject), "Awake")]
+        [HarmonyPrefix]
+        public static void Patch()
+        {
+            Debug.Log("asd");
+        }
+
+        private void Update()
+        {
+            Debug.Log("asdasd");
+        }
+
+        private void Start()
+        {
+            Harmony.CreateAndPatchAll(typeof(BoltPatch));
+        }
+    }
+}
---
+++ Plugin.cs
@@ -0,0 +1,15 @@
+using BepInEx;
+using UnityEngine;
+
+namespace BoltActionFix
+{
+    [BepInPlugin("asd", "adsads", "1.0.0")]
+    [BepInProcess("h3vr.exe")]
+    public class Plugin : BaseUnityPlugin
+    {
+        private void Awake()
+        {
+            Debug.Log("Beep");
+        }
+    }
+}
